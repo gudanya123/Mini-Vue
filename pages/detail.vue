2f60781e4f723cfca7d302da7931e3d4ab7ff70c
Bin Yao <18100174744@163.com>
2022-03-14 20:33:13 +0800

    vue-nuxt

---
+++ pages/detail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h2>detail page</h2>
+
+      <!-- nuxt-child表示嵌套关系 -->
+      <!-- 类似rooter-view -->
+      <nuxt-child></nuxt-child>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ server/api.js
@@ -0,0 +1,60 @@
+// 此文件并非nuxt生成，它为演示项目提供数据服务接口
+const Koa = require('koa');
+const app = new Koa();
+const bodyparser = require('koa-bodyparser');
+const router = require('koa-router')({ prefix: '/api' });
+
+//设置cookie加密钥匙
+app.keys = ['some secret', 'another secret'];
+
+const goods = [
+    { id: 1, text: 'Web全栈架构师', price: 10000 },
+    { id: 2, text: 'Java全栈架构师', price: 12000 }
+
+];
+
+//配置路由
+//获取产品列表
+router.get('/goods', ctx => {
+    ctx.body = {
+        ok:1,
+        goods
+    }
+})
+
+//获取产品详情
+router.get('/detail', ctx => {
+    ctx.body = {
+        ok:1,
+        data: goods.find(good => good.id == ctx.query.id)
+    }
+})
+
+//登录
+router.post('/login', ctx => {
+    const user = ctx.request.body;
+    if (user.username === 'admin' && user.password === 'admin') {
+        //将token存入cookie
+        const token = 'a mock token';
+        ctx.cookies.set('token', token);
+        ctx.body = {
+            ok:1,
+            token
+        }
+    } else {
+        ctx.body = {
+            ok: -1,
+            msg: '登录失败',
+        }
+    }
+});
+
+//解析post数据并注册路由
+app.use(bodyparser());
+
+//注册路由
+app.use(router.routes());
+
+app.listen(8080,()=>{
+    console.log('api is running at 8080');
+})
